8232a3bcee63929fb138ffa0ea11d6bab62a5d8f
Fadhel Khalil <khalil.fadhel@gmail.com>
2012-11-24 22:58:29 +0100

    Files Uploaded

---
+++ slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Unboxing the WEB',
+    subtitle: 'Unleash the web',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-32630298-1',
+    favIcon: 'images/logonetlinks.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Inconsolata',
+      'Cabin Sketch:400',
+      'Muli'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Fadhel Khalil',
+    gplus: 'http://www.facebook.com/khalilfadhel',
+    title: 'GL2 Student, designer and web developper',
+    company: 'Netlinks',
+    twitter: '@khf118',
+    www: 'www.netlinks-insat.com',
+    github: ''
+  }]
+};
+
